4ca532020d65f10aed3e754b79b18d404059fbb8
rimshabaloch <109247278+rimshabaloch@users.noreply.github.com>
2022-07-14 03:29:04 +0500

    My Portfolio Website

---
+++ public/js/custom.js
@@ -0,0 +1,10 @@
+
+// NIVO LIGHTBOX
+$('#portfolio a').nivoLightbox({
+        effect: 'fadeScale',
+    });
+
+// HIDE MOBILE MENU AFTER CLIKING ON A LINK
+   $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
